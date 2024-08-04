9cd538830001409517f2c5f6b12b5c060adaca17
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-08-03 21:16:33 -0300

    Update Ejercicio8A.asm

--- Practicas/Practica 1/Ejercicio8A.asm
+++ Practicas/Practica 1/Ejercicio8A.asm
@@ -1,4 +1,5 @@
-# Escribir una subrutina LONGITUD que cuente el número de caracteres de una cadena de caracteres terminada en cero (00H) almacenada en la memoria. 
+# Escribir una subrutina LONGITUD que cuente el número de caracteres de una cadena de caracteres terminada en cero (00H) almacenada en 
+la memoria. 
 La cadena se pasa a la subrutina por referencia vía registro, y el resultado se retorna por valor también a través de un registro.
 Ejemplo: la longitud de ‘abcd’00h es 4 (el 00h final no cuenta)
 
