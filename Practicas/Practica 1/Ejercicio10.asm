d10e55f50902dd5f71c7e547921da9f005ee6f15
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-08-03 21:03:52 -0300

    Update Ejercicio10.asm

--- Practicas/Practica 1/Ejercicio10.asm
+++ Practicas/Practica 1/Ejercicio10.asm
@@ -1,5 +1,5 @@
-# SWAP Escribir una subrutina SWAP que intercambie dos datos de 16 bits almacenados en memoria. Los parámetros deben ser pasados por referencia desde 
-el programa principal a través de la pila.
+# SWAP Escribir una subrutina SWAP que intercambie dos datos de 16 bits almacenados en memoria. Los parámetros deben ser pasados por 
+referencia desde el programa principal a través de la pila.
 Para hacer este ejercicio, tener en cuenta que los parámetros que se pasan por la pila son las direcciones de memoria, por lo tanto para acceder 
 a los datos a intercambiar se requieren accesos indirectos, además de los que ya se deben realizar para acceder a los parámetros de la pila.
 
