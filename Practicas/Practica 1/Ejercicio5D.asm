b10c388fe5e5776d5ccb2c774abb0cc54a455bb0
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-08-04 18:08:47 -0300

    Create Ejercicio5D.asm

---
+++ Practicas/Practica 1/Ejercicio5D.asm
@@ -0,0 +1,5 @@
+# Si tuviera que realizar el cálculo dos veces con números distintos, por ejemplo, unos guardados en variables A1, B1, C1 y otros 
+# guardados en variables A2, B2, C2, ¿podrían reutilizarse las subrutinas del inciso b) sin modificarse? ¿y las del inciso c?
+
+# En el inciso B al no haber subrutina no hay reutilización de codigo, ya que, todo el codigo esta escrito en el programa principal y en 
+# el inciso C al haber subrutina podes reutilizar una parte del codigo.
